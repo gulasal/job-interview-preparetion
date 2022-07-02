35f90b8b0be4a446658e14faeb15ba3cabb68da1
Gulasal Kuchkarova <gulasal.kuchkarova@stud.th-deg.de>
2022-07-02 23:57:50 +0200

    with_sample

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,7 +1,6 @@
 #Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-nums = [1,3,5,6]
-target = 5
+
 def searchInsert( nums, target: int) -> int:
         for num in nums:
             if (target == 0):
@@ -13,3 +12,5 @@ def searchInsert( nums, target: int) -> int:
             if ((num != target) and (num ==(target + 1))):
                 nums.insert(0, 0)
                 return(nums.index(num)-1)
+
+print(searchInsert([1,3,5,6],5))
