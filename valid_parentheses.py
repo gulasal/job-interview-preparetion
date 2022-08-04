a535232b1e0f18105d2073c79ce715bff1a990cd
Gulasal Kuchkarova <gulasal.kuchkarova@stud.th-deg.de>
2022-08-04 20:58:09 +0200

    code corrected

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,17 +1,23 @@
 def isValid(s):
-        
-    for i in s:
-        if '()' in s:
-            s= s.replace( '()','')
-        elif '{}' in s :
-            s = s.replace( '[]', '')
-        elif '[]' in s :
-            s = s.replace ( '{}', '')
-            break
-        else :
-            return not s
-s ='({[]})'
+        if len(s) % 2 != 0:
+            return False
+        dict = {'(' : ')', '[' : ']', '{' : '}'}
+        stack = []
+        for i in s:
+            if i in dict.keys():
+                stack.append(i)
+            else:
+                if stack == []:
+                    return False
+                a = stack.pop()
+                if i!= dict[a]:
+                    return False
+        return stack == []
+s =['(', ')']
+s2 =['{', '(', ')']
 print(isValid(s))
+print(isValid(s2))
+
 
     
     
